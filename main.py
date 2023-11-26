4e5675ffb685d09957026b62090b0cacb9bb0afb
goose <diomns2006@yandex.ru>
2023-11-26 11:25:33 +0300

    Add enter

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from src import auth_router, navig_router, server_router, docker_router, vm_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
 app.mount("/src/static", StaticFiles(directory="src/static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(navig_router)
--- src/static/js/form.js
+++ src/static/js/form.js
@@ -20,3 +20,32 @@ passInput.addEventListener("blur", function() {
 		this.placeholder = "Пароль";
 	}
 });
+
+const loginForm = document.getElementById("form__id");
+const loginButton = document.getElementById("button");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = document.getElementById('usernameInput').value;
+    const password = document.getElementById('passwordInput').value;
+
+    fetch("http://localhost:8000/user/add", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        localStorage.setItem("token", data.token);
+
+        window.location.href = "/main";
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+});
